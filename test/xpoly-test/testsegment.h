48e3ac61623b79dc9d27ecef4a684c9d7048cd92
capalvarez <capalvarez.i@gmail.com>
2016-05-26 11:16:36 -0400

    Organization of code, inclution of gtest to start testing code.

---
+++ test/xpoly-test/testsegment.h
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+class SegmentTest: public ::testing::TEST{
+protected:
+    virtual void SetUp(){
+        Point* p1 = new Point(0,0);
+        Point* p2 = new Point(1,1);
+
+        segment_points = {*p1,*p2};
+        segment = *new Segment(0,1);
+    }
+
+    std::vector<Point> segment_points;
+    Segment segment;
+}
+
