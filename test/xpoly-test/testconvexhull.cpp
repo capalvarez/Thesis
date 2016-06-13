41adb040a1b8b21e1768e455ad6232225b8235cb
capalvarez <capalvarez.i@gmail.com>
2016-06-13 12:15:36 -0400

    Tests convexhull on several cases, rotating calipers in progress.

--- test/xpoly-test/testconvexhull.cpp
+++ test/xpoly-test/testconvexhull.cpp
@@ -1,8 +1,6 @@
-#include <gtest/gtest.h>
-#include <geometry/Point.h>
-#include <utilities/convexHull.h>
+#include "testconvexhull.h"
 
-TEST(ConvexHullTest, OrientationTest){
+TEST_F(ConvexHullTest, OrientationTest){
     std::vector<Point> points = {Point(2,0),Point(5,5),Point(0,3),Point(0,0),
                                 Point(1,2),Point(0.5,1),Point(0,2)};
     EXPECT_EQ(convex::orientation(points[0],points[1],points[2]), 19);
@@ -10,21 +8,53 @@ TEST(ConvexHullTest, OrientationTest){
     EXPECT_EQ(convex::orientation(points[4],points[5],points[6]), -1);
 }
 
-TEST(ConvexHullTest, ConvexHullFunctionTest){
-    std::vector<Point> points = {Point(0,2), Point(3,4), Point(6,2), Point(3,0), Point(2,2), Point(4,1), Point(4,3)};
+TEST_F(ConvexHullTest, ConvexHullFunctionTest){
     std::vector<Point> upper;
     std::vector<Point> lower;
 
-    convex::convexHull(points,upper,lower);
+    convex::convexHull(diamond,upper,lower);
 
     std::vector<Point> expectedU = {Point(0,2), Point(3,4), Point(6,2)};
     std::vector<Point> expectedL = {Point(0,2), Point(3,0), Point(6,2)};
 
     EXPECT_EQ(upper,expectedU);
     EXPECT_EQ(lower, expectedL);
+
+    upper.clear();
+    lower.clear();
+
+    convex::convexHull(triangle,upper,lower);
+
+    expectedU.clear();
+    expectedL.clear();
+
+    expectedU = {Point(0,0), Point(0,1), Point(1,0)};
+    expectedL = {Point(0,0), Point(1,0)};
+
+    EXPECT_EQ(upper,expectedU);
+    EXPECT_EQ(lower,expectedL);
+
+    upper.clear();
+    lower.clear();
+
+    convex::convexHull(nonconvex,upper,lower);
+
+    expectedU.clear();
+    expectedL.clear();
+
+    expectedU = {Point(0,0),Point(1,2),Point(2,0)};
+    expectedL = {Point(0,0), Point(2,0)};
+
+    EXPECT_EQ(upper,expectedU);
+    EXPECT_EQ(lower,expectedL);
 }
 
-TEST(ConvexHullTest, RotatingCalipersTest){
+TEST_F(ConvexHullTest, RotatingCalipersTest){
+    std::vector<std::pair<Point,Point> > diamondRot = convex::rotatingCalipers(diamond);
+    std::vector<std::pair<Point,Point> > diamondRes;
+    diamondRes.push_back(std::make_pair(Point(0,2),Point(6,2)));
+    diamondRes.push_back(std::make_pair(Point(3,4),Point(3,0)));
 
+    EXPECT_EQ(diamondRot,diamondRes);
 }
 
---
+++ test/xpoly-test/testconvexhull.h
@@ -0,0 +1,24 @@
+#ifndef THESIS_TESTCONVEXHULL_H
+#define THESIS_TESTCONVEXHULL_H
+
+#include <gtest/gtest.h>
+#include <geometry/Point.h>
+#include <utilities/convexHull.h>
+#include <vector>
+
+class ConvexHullTest : public ::testing::Test{
+protected:
+    virtual void SetUp(){
+        diamond = {Point(0,2), Point(3,4), Point(6,2), Point(3,0), Point(2,2), Point(4,1), Point(4,3)};
+        triangle = {Point(0,0), Point(1,0), Point(0,1)};
+        nonconvex = {Point(0,0), Point(1,1), Point(2,0), Point(1,2)};
+    }
+
+    std::vector<Point> diamond;
+    std::vector<Point> triangle;
+    std::vector<Point> nonconvex;
+
+};
+
+
+#endif
