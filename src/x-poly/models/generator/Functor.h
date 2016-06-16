08a6087094c3ebe567e19d097f66477d1901b31f
capalvarez <capalvarez.i@gmail.com>
2016-06-16 13:15:22 -0400

    Functor structure from point generation.

---
+++ src/x-poly/models/generator/Functor.h
@@ -0,0 +1,10 @@
+#ifndef THESIS_FUNCTOR_H
+#define THESIS_FUNCTOR_H
+
+class Functor {
+public:
+    virtual double operator()(double x) = 0;
+};
+
+
+#endif 
--- test/xpoly-test/models/generator/testpointgenerator.h
+++ test/xpoly-test/models/generator/testpointgenerator.h
@@ -7,6 +7,8 @@
 
 class PointGeneratorTest : public ::testing::Test {
 protected:
+    Rectangle* box = new Rectangle(Point(0,0), Point(10,10));
+    PointGenerator* const_const = new PointGenerator(functions::constant(), functions::constant());
 
 };
 
