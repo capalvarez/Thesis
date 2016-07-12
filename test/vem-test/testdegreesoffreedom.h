f81425cd22521149bbeff2b4efa0a7a804bdd0f5
Catalina Alvarez <capalvarez.i@gmail.com>
2016-07-12 14:20:31 -0400

    add tests for vemlib

---
+++ test/vem-test/testdegreesoffreedom.h
@@ -0,0 +1,25 @@
+#ifndef THESIS_TESTDEGREESOFFREEDOM_H
+#define THESIS_TESTDEGREESOFFREEDOM_H
+
+#include <gtest/gtest.h>
+#include <x-poly/models/polygon/Polygon.h>
+#include <matrix/DegreesOfFreedom.h>
+
+class DegreesOfFreedomTest : public ::testing::Test{
+protected:
+    virtual void SetUp(){
+        points = {Point(0,0), Point(1,0), Point(1,1), Point(0,1)};
+        std::vector<int> point_index = {0, 1, 2, 3};
+        Polygon p (point_index, points);
+
+        degreesSquare = new DegreesOfFreedom(p, 2, points);
+
+    }
+
+    std::vector<Point> points;
+    DegreesOfFreedom* degreesSquare;
+
+};
+
+
+#endif
