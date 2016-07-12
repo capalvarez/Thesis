97969509ab90a7f5fd93783b6a0ce9350883d2f8
Catalina Alvarez <capalvarez.i@gmail.com>
2016-07-12 14:18:21 -0400

    Can integrate on a triangle :)

--- src/vem/models/VEMTriangle.cpp
+++ src/vem/models/VEMTriangle.cpp
@@ -1,2 +1,42 @@
 #include "VEMTriangle.h"
+#include "gaussQuadrature.cpp"
+
+VEMTriangle::VEMTriangle(Point p1, Point p2, Point p3) {
+    this->p1 = p1;
+    this->p2 = p2;
+    this->p3 = p3;
+}
+
+double VEMTriangle::integrate(func_t f) {
+    int order = 2;
+    double result = 0;
+
+    std::vector<Point> quad_p;
+    std::vector<double> weight;
+
+    gauss::gaussQuadrature(order,quad_p,weight);
+
+    for(int i=0;i<weight.size();i++){
+        double P = p1.getX() + (p2.getX() - p1.getX())*quad_p[i].getX() + (p3.getX() - p1.getX())*quad_p[i].getY();
+        double Q = p1.getY() + (p2.getY() - p1.getY())*quad_p[i].getX() + (p3.getY() - p1.getY())*quad_p[i].getY();
+
+        double r = f(P,Q);
+        result += weight[i]*r;
+    }
+
+    return result*jacobian()/2;
+}
+
+
+double VEMTriangle::jacobian() {
+    Eigen::Matrix2f m;
+    m << p2.getX() - p1.getX(), p3.getX() - p1.getX(),
+         p2.getY() - p1.getY(), p3.getY() - p1.getY();
+
+    return m.determinant();
+}
+
+
+
+
 
--- src/vem/models/VEMTriangle.h
+++ src/vem/models/VEMTriangle.h
@@ -2,10 +2,21 @@
 #define THESIS_TRIANGLE_H
 
 #include <x-poly/models/polygon/Triangle.h>
+#include <x-poly/models/basic/Point.h>
+#include <lib/Eigen/Dense>
+
+typedef double(*func_t)(double, double);
 
 class VEMTriangle {
+private:
+    Point p1;
+    Point p2;
+    Point p3;
 
-
+    double jacobian();
+public:
+    VEMTriangle(Point p1, Point p2, Point p3);
+    double integrate(func_t f);
 };
 
 
