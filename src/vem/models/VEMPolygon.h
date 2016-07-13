cd7f6674c0c840d95cfda6388e77e334e470ec4d
capalvarez <capalvarez.i@gmail.com>
2016-07-13 18:09:56 -0400

    Leftovers: ear clipping algorithm pending

--- src/vem/models/VEMPolygon.h
+++ src/vem/models/VEMPolygon.h
@@ -8,6 +8,8 @@
 typedef double(*func_t)(double, double);
 
 class VEMPolygon : public Polygon{
+private:
+    VEMTriangle getEar(std::vector<Point> points);
 public:
     VEMPolygon(std::vector<int> index, std::vector<Point> points);
     std::vector<VEMTriangle> triangulate(std::vector<VEMTriangle>& triangles, std::vector<Point>& points);
