fcff9ff870785ee07722cad786cd6d6966f6ec41
capalvarez <capalvarez.i@gmail.com>
2016-07-20 16:17:16 -0400

    Everything works, branch closed

--- test/xpoly-test/models/generator/testpointgenerator.cpp
+++ test/xpoly-test/models/generator/testpointgenerator.cpp
@@ -7,4 +7,13 @@ TEST_F(PointGeneratorTest, GenerateTest){
     std::vector<Point> exp = {Point(0,0),Point(5,0),Point(10,0),Point(0,5),Point(5,5),Point(10,5),
                               Point(0,10),Point(5,10),Point(10,10)};
     EXPECT_EQ(res, exp);
+
+    /*Rectangle* box = new Rectangle(Point(0,0), Point(1,1));
+    PointGenerator* ran_ran = new PointGenerator(functions::random_double(0,1), functions::random_double(0,1));
+    std::vector<Point> p;
+    ran_ran->generate(p,*box,3,3);
+
+    for(int i=0;i<9;i++){
+        std::cout << p[i].getString() << std::endl;
+    }*/
 }
