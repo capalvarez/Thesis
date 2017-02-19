764a37aed94c52cf3dc9a48aef8f62982c5ff5db
capalvarez <capalvarez.i@gmail.com>
2017-02-19 12:56:35 -0300

    Changes in material

--- veamy/src/physics/Material.cpp
+++ veamy/src/physics/Material.cpp
@@ -14,7 +14,7 @@ Eigen::MatrixXd Material::getMaterialMatrix() {
 
     D(0,0) = c*(1 - this->v);
     D(1,1) = c*(1 - this->v);
-    D(2,2) = 2*c*(1 - this->v);
+    D(2,2) = 2*c*(1 - 2*this->v);
 
     return D;
 }
@@ -22,7 +22,7 @@ Eigen::MatrixXd Material::getMaterialMatrix() {
 double Material::trace() {
     double c = (this->E)/((1 + this->v)*(1 - 2*this->v));
 
-    return 4*c*(1 - this->v);
+    return 2*c*((1 - this->v)+(1 - 2*this->v));
 }
 
 double Material::stressIntensityFactor() {
